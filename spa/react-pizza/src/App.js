c5ea74afdfb68e7e27ddd6e3b8c7e0a70b6ab056
mars <maspam@yandex.ru>
2021-08-02 22:38:39 +0300

    added react-pizza project

---
+++ spa/react-pizza/src/App.js
@@ -0,0 +1,18 @@
+import { Route } from 'react-router-dom';
+import { Header } from './components';
+import { Cart, Home } from './pages';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <Header />
+
+      <div className='content'>
+        <Route path='/' component={Home} exact />
+        <Route path='/cart' component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
